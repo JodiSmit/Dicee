c95552020ff7be607dbc1bae9604b5cfcfba473c
Jodi Smit <jodi@nebulahome.com>
2019-04-10 11:17:40 -0400

    Created simple app to roll dice
    Has Button to roll dice and also a shake function to roll the dice.

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	private var randomDiceIndex1 = 0
+	private var randomDiceIndex2 = 0
+	
+	private let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
+	@IBOutlet weak var firstDiceImage: UIImageView!
+	@IBOutlet weak var secondDiceImage: UIImageView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		updateDiceImages()
 	}
 
-
+	@IBAction func rollButtonTapped(_ sender: Any) {
+		updateDiceImages()
+	}
+	
+	override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+		updateDiceImages()
+	}
+	private func updateDiceImages() {
+		
+		randomDiceIndex1 = Int.random(in: 0...5)
+		randomDiceIndex2 = Int.random(in: 0...5)
+		
+		firstDiceImage.image = UIImage(named: diceArray[randomDiceIndex1])
+		secondDiceImage.image = UIImage(named: diceArray[randomDiceIndex2])
+	}
+	
 }
 
